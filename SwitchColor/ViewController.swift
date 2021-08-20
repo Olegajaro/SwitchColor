f4ea5164e3e170ee0706382b2b8de3699799e870
Olegajaro <88854903+Olegajaro@users.noreply.github.com>
2021-08-21 00:28:17 +0300

    Added logic in ViewController.swift

--- SwitchColor/ViewController.swift
+++ SwitchColor/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var mutableView: UIView!
+    
+    @IBOutlet var valueRedSlider: UILabel!
+    @IBOutlet var valueGreenSlider: UILabel!
+    @IBOutlet var valueBlueSlider: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        mutableView.layer.cornerRadius = 10
+    }
+    
+    func changeColor() {
+        mutableView.backgroundColor = UIColor(
+            displayP3Red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value),
+            alpha: 1)
     }
 
-
+    
+    @IBAction func changingRedAlphaComponent() {
+        valueRedSlider.text = String(format: "%.2f", redSlider.value)
+        
+        changeColor()
+    }
+    
+    @IBAction func changingGreenAlphaComponent() {
+        valueGreenSlider.text = String(format: "%.2f", greenSlider.value)
+        
+        changeColor()
+    }
+    
+    @IBAction func changingBlueAlphaComponent(_ sender: Any) {
+        valueBlueSlider.text = String(format: "%.2f", blueSlider.value)
+        
+        changeColor()
+    }
 }
 
