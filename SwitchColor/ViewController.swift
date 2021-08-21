c30163009a91dd5646c8953758485721815ff182
Olegajaro <88854903+Olegajaro@users.noreply.github.com>
2021-08-21 19:19:49 +0300

    Final version

--- SwitchColor/ViewController.swift
+++ SwitchColor/ViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: - IB Outlets
     @IBOutlet var mutableView: UIView!
     
     @IBOutlet var valueRedSlider: UILabel!
@@ -18,21 +19,14 @@ class ViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
-
+    // MARK: - Life Cicles Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         
         mutableView.layer.cornerRadius = 10
     }
     
-    func changeColor() {
-        mutableView.backgroundColor = UIColor(
-            displayP3Red: CGFloat(redSlider.value),
-            green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value),
-            alpha: 1)
-    }
-
-    
+    // MARK: - IB Actions
     @IBAction func changingRedAlphaComponent() {
         valueRedSlider.text = String(format: "%.2f", redSlider.value)
         
@@ -52,3 +46,12 @@ class ViewController: UIViewController {
     }
 }
 
+// MARK: - Private Methods
+extension ViewController {
+    private func changeColor() {
+        mutableView.backgroundColor = UIColor(
+            displayP3Red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value),
+            alpha: 1)
+    }
+}
