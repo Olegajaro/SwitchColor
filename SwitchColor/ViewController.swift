f065cd9d9f5e65368228cc59f46b6f2a22d77acd
Olegajaro <88854903+Olegajaro@users.noreply.github.com>
2021-08-27 13:16:23 +0300

    Optimization logic #1

--- SwitchColor/ViewController.swift
+++ SwitchColor/ViewController.swift
@@ -32,32 +32,29 @@ class ViewController: UIViewController {
         valueBlueSlider.text = String(format: "%.2f", blueSlider.value)
         
         mutableView.layer.cornerRadius = 10
-        mutableView.backgroundColor = UIColor(
-            displayP3Red: CGFloat(redSlider.value),
-            green: CGFloat(greenSlider.value),
-            blue: CGFloat(blueSlider.value),
-            alpha: 1
-        )
-    }
-    
-    // MARK: - IB Actions
-    @IBAction func changingColorMutableView() {
-        valueRedSlider.text = String(format: "%.2f", redSlider.value)
-        valueGreenSlider.text = String(format: "%.2f", greenSlider.value)
-        valueBlueSlider.text = String(format: "%.2f", blueSlider.value)
         
         changeColor()
     }
-}
-
-// MARK: - Private Methods
-extension ViewController {
+    
+    // MARK: - IB Actions
+    
+    @IBAction func rgbSlider(_ sender: UISlider) {
+        changeColor()
+        
+        valueRedSlider.text = String(format: "%.2f", redSlider.value)
+        valueGreenSlider.text = String(format: "%.2f", greenSlider.value)
+        valueBlueSlider.text = String(format: "%.2f", blueSlider.value)
+    }
+    
+    // MARK: - Private Methods
     private func changeColor() {
         mutableView.backgroundColor = UIColor(
-            displayP3Red: CGFloat(redSlider.value),
+            red: CGFloat(redSlider.value),
             green: CGFloat(greenSlider.value),
             blue: CGFloat(blueSlider.value),
-            alpha: 1
-        )
+            alpha: 1)
     }
 }
+
+
+
